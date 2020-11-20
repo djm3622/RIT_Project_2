b013085cf9157035ba5e82e76a625592a072f3b1
djm3622 <djm3622@rit.edu>
2020-11-20 15:10:32 -0500

    part 1 mostly done

--- src/model/RITMain.java
+++ src/model/RITMain.java
@@ -1,30 +1,41 @@
 package model;
 
-import javafx.scene.canvas.GraphicsContext;
-import javafx.scene.paint.Color;
-
 import java.util.ArrayList;
-import java.util.LinkedList;
+import java.util.Arrays;
 import java.util.List;
 
 
 public class RITMain {
-
     /** the observers of this model */
-    private final List<Observer<RITMain>> observers;
+    private List<Observer<RITMain>> observers;
 
-    private final ArrayList<Integer> arr_i;
-    private final int DIM;
+    private ArrayList<Integer> arr_i;
+    private int DIM;
     private int[][] imageMatrix;
+    private final int selection;
+    private int depth;
+    private RITQTNode node;
+    private ArrayList<ArrayList<Integer>> arrayList;
 
     /**
      * Create a new board.
      */
-    public RITMain(ArrayList<Integer> arr_i) {
-        this.observers = new LinkedList<>();
-        this.arr_i = arr_i;
-        this.DIM = (int) Math.sqrt(arr_i.size());
-        this.imageMatrix = new int[(int) this.DIM][(int) this.DIM];
+    public RITMain(ArrayList<Integer> arr_i, int selection) {
+        this.selection = selection;
+        switch (selection) {
+            case (0) -> {
+                this.arr_i = arr_i;
+                this.DIM = (int) Math.sqrt(arr_i.size());
+                this.imageMatrix = new int[(int) this.DIM][(int) this.DIM];
+            }
+            case (1) -> {
+                this.depth = arr_i.remove(0);
+                this.DIM = (int) Math.sqrt(this.depth);
+                this.imageMatrix = new int[(int) this.DIM][(int) this.DIM];
+                this.node = parse(arr_i);
+                createImageMatrix(this.node, 0, 0, this.DIM);
+            }
+        }
     }
 
     public int getDIM() {
@@ -38,7 +49,7 @@ public class RITMain {
     public void createImageMatrix() {
         int y = 0;
         int x = 0;
-        for (int e: this.arr_i) {
+        for (int e : this.arr_i) {
             if (x == this.DIM) {
                 y++;
                 x = 0;
@@ -48,22 +59,63 @@ public class RITMain {
         }
     }
 
-    /**
-     * The view calls this method to add themselves as an observer of the model.
-     *
-     * @param observer the observer
-     */
-    public void addObserver(Observer<RITMain> observer) {
-        this.observers.add(observer);
-    }
+    public void createImageMatrix(RITQTNode node, int row, int col, int dim) {
+        RITQTNode temp_ul = node.getUpperLeft();
+        if (temp_ul.getVal() == -1) {
+            createImageMatrix(temp_ul, row, col,dim / 2);
+        } else {
+            buildImagineMatrix(row, col, dim / 2, temp_ul.getVal());
+        }
 
-    /** When the model changes, the observers are notified via their update() method */
-    private void notifyObservers() {
-        for (Observer<RITMain> obs: this.observers ) {
-            obs.update(this);
+        RITQTNode temp_ur = node.getUpperRight();
+        if (temp_ur.getVal() == -1) {
+            createImageMatrix(temp_ur, row, col + (dim / 2),dim / 2);
+        } else {
+            buildImagineMatrix(row, col + (dim / 2), dim / 2, temp_ur.getVal());
+        }
+
+        RITQTNode temp_ll = node.getLowerLeft();
+        if (temp_ll.getVal() == -1) {
+            createImageMatrix(temp_ll, row + (dim / 2), col,dim / 2);
+        } else {
+            buildImagineMatrix(row + (dim / 2), col, dim / 2, temp_ll.getVal());
+        }
+
+        RITQTNode temp_lr = node.getLowerRight();
+        if (temp_lr.getVal() == -1) {
+            createImageMatrix(temp_lr, row + (dim / 2), col + (dim / 2),dim / 2);
+        } else {
+            buildImagineMatrix(row + (dim / 2), col  + (dim / 2), dim / 2, temp_lr.getVal());
         }
     }
 
+    private void buildImagineMatrix(int row, int col, int dim, int val) {
+        for (int x = col; x < dim+col; x++) {
+            for (int y = row; y < dim+row; y++) {
+                this.imageMatrix[x][y] = val;
+            }
+        }
+    }
+
+    /**
+     *
+     * check for the dimensions
+     *
+     * @param num the dimesnions
+     * @return whether it is a square or not
+     */
+    public boolean checkForSquare(int num) {
+        int check = 1;
+        while (true) {
+            if (num == check) {
+                return true;
+            } else if (num < check) {
+                return false;
+            } else {
+                check *= 2;
+            }
+        }
+    }
 
     public RITQTNode parse(List<Integer> arr) {
         int arr_e = arr.remove(0);
